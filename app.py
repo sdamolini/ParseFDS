9bac9f0570556cd12f7c193559b989a828b3e582
Stephane <48075404+sdamolini@users.noreply.github.com>
2020-06-13 12:11:59 -0400

    Push III

--- app.py
+++ app.py
@@ -1,37 +1,37 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
-# import pickle
+import argparse
+from werkzeug.utils import secure_filename
+
 
 # create flask app
 app = Flask(__name__)
 
-# # load model
-# model = pickle.load(open('model.pkl', 'rb'))
+UPLOAD_FOLDER = 'uploads/'
+MAX_CONTENT_LENGTH = 500*1000*1000
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = MAX_CONTENT_LENGTH
 
 # home page
 @app.route('/')
 def home():
-    return render_template('index.html')
+   return render_template('index.html')
 
-# post page
-@app.route('/predict', methods=['POST'])
-def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-
-    # extract fields features
-    int_features = [int(x) for x in request.form.values()]
-    # store features for predictions
-    final_features = [np.array(int_features)]
-    # make predictions
-    prediction = model.predict(final_features)
-    # format output
-    output = round(prediction[0], 2)
-
-    # render index with adder inputs
-    output = "Employee Salary should be $ {0}\n\nExperience={1}\nTest Score={2}\nInterview Score={3}".format(output,*int_features).split('\n')
-    return render_template('index.html', prediction_text=output)
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save(secure_filename(f.filename))
+      return 'file uploaded successfully'
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    ## parse arguments for debug mode
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-d", "--debug", action="store_true", help="debug flask")
+    args = vars(ap.parse_args())
+
+    if args["debug"]:
+        app.run(debug=True, port=8080)
+    else:
+        app.run(host='0.0.0.0', threaded=True ,port=8080)
